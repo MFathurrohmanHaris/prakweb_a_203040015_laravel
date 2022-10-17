be15842491988c468373aa11c8f8ba0b4e58fd86
MFathurrohmanHaris <harisfathurrohman142@gmail.com>
2022-10-17 14:33:35 +0700

    mengerjakan video #3 Struktur folder, Routes & view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Muhammad Fathurrohman Haris",
+        "email" => "harisfathurrohman142@gmail.com",
+        "image" => "haris.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
