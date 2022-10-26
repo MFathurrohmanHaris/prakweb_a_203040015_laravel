1af2aee1e6cd5ad6921d933f03d91cf1dcb40aec
MFathurrohmanHaris <harisfathurrohman142@gmail.com>
2022-10-26 23:07:20 +0700

    mengerjakan video#9 Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,63 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Muhammad Fathurrohman Haris',
+            'email' => 'haris@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Muhammad Imam Ansori',
+            'email' => 'imamansori@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' =>  'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias esse delectus incidunt dicta labore tempora cum.',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias esse delectus incidunt dicta labore tempora cum. Omnis vero pariatur nostrum eaque rem, eveniet error labore dolorum? Quae vitae non, dolorem numquam ipsa alias odit consequuntur incidunt ipsum, fuga quo deserunt, nam error! Eligendi magnam non expedita tempora qui labore aut? Architecto minus, maiores enim porro ullam accusantium sed. Ipsam consequuntur quis ab architecto. Sequi ea possimus impedit similique veritatis provident fugit, doloribus esse, dolorem, perferendis vero soluta nostrum enim quas veniam voluptates quod fugiat numquam pariatur omnis assumenda nihil? Placeat ex, officiis impedit quod fugit excepturi quibusdam voluptates laboriosam tenetur.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias esse delectus incidunt dicta labore tempora cum.porro ullam accusantium sed. Ipsam consequuntur quis ab architecto. Sequi ea possimus impedit similique veritatis provident fugit, doloribus esse, dolorem, perferendis vero soluta nostrum enim quas veniam voluptates quod fugiat numquam pariatur omnis assumenda</p>',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' =>  'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias esse delectus incidunt dicta labore tempora cum.',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias esse delectus incidunt dicta labore tempora cum. Omnis vero pariatur nostrum eaque rem, eveniet error labore dolorum? Quae vitae non, dolorem numquam ipsa alias odit consequuntur incidunt ipsum, fuga quo deserunt, nam error! Eligendi magnam non expedita tempora qui labore aut? Architecto minus, maiores enim porro ullam accusantium sed. Ipsam consequuntur quis ab architecto. Sequi ea possimus impedit similique veritatis provident fugit, doloribus esse, dolorem, perferendis vero soluta nostrum enim quas veniam voluptates quod fugiat numquam pariatur omnis assumenda nihil? Placeat ex, officiis impedit quod fugit excepturi quibusdam voluptates laboriosam tenetur.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias esse delectus incidunt dicta labore tempora cum.porro ullam accusantium sed. Ipsam consequuntur quis ab architecto. Sequi ea possimus impedit similique veritatis provident fugit, doloribus esse, dolorem, perferendis vero soluta nostrum enim quas veniam voluptates quod fugiat numquam pariatur omnis assumenda</p>',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' =>  'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias esse delectus incidunt dicta labore tempora cum.',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias esse delectus incidunt dicta labore tempora cum. Omnis vero pariatur nostrum eaque rem, eveniet error labore dolorum? Quae vitae non, dolorem numquam ipsa alias odit consequuntur incidunt ipsum, fuga quo deserunt, nam error! Eligendi magnam non expedita tempora qui labore aut? Architecto minus, maiores enim porro ullam accusantium sed. Ipsam consequuntur quis ab architecto. Sequi ea possimus impedit similique veritatis provident fugit, doloribus esse, dolorem, perferendis vero soluta nostrum enim quas veniam voluptates quod fugiat numquam pariatur omnis assumenda nihil? Placeat ex, officiis impedit quod fugit excepturi quibusdam voluptates laboriosam tenetur.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias esse delectus incidunt dicta labore tempora cum.porro ullam accusantium sed. Ipsam consequuntur quis ab architecto. Sequi ea possimus impedit similique veritatis provident fugit, doloribus esse, dolorem, perferendis vero soluta nostrum enim quas veniam voluptates quod fugiat numquam pariatur omnis assumenda</p>',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' =>  'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias esse delectus incidunt dicta labore tempora cum.',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias esse delectus incidunt dicta labore tempora cum. Omnis vero pariatur nostrum eaque rem, eveniet error labore dolorum? Quae vitae non, dolorem numquam ipsa alias odit consequuntur incidunt ipsum, fuga quo deserunt, nam error! Eligendi magnam non expedita tempora qui labore aut? Architecto minus, maiores enim porro ullam accusantium sed. Ipsam consequuntur quis ab architecto. Sequi ea possimus impedit similique veritatis provident fugit, doloribus esse, dolorem, perferendis vero soluta nostrum enim quas veniam voluptates quod fugiat numquam pariatur omnis assumenda nihil? Placeat ex, officiis impedit quod fugit excepturi quibusdam voluptates laboriosam tenetur.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias esse delectus incidunt dicta labore tempora cum.porro ullam accusantium sed. Ipsam consequuntur quis ab architecto. Sequi ea possimus impedit similique veritatis provident fugit, doloribus esse, dolorem, perferendis vero soluta nostrum enim quas veniam voluptates quod fugiat numquam pariatur omnis assumenda</p>',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.main')
 
 @section('container')
-  <h1>{{ $post->title }}</h1>
+  <h1 class="mb-5">{{ $post->title }}</h1>
 
-  <p>By. MF Haris <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+  <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
   
   {!! $post->body !!}
 
-<a href="/posts">Back To Blog</a>
+<a href="/posts" class="d-block mt-3">Back To Blog</a>
 
 @endsection
